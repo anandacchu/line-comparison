7a093457263dce295e5636db892c1829e9a29b32
anand acchu <you@example.com>
2022-08-15 18:07:55 +0530

    welcome

---
+++ uc1.cs
@@ -0,0 +1,23 @@
+﻿
+
+namespace Project_num1
+{
+    internal class UC1
+
+    {
+        int x1 = 1;
+        int y1 = 2;
+        int x2 = 3;
+        int y2 = 4;
+
+        double LengthLine = 0;
+
+        public void CartesianSystem()
+        {
+            LengthLine = ((x2 - x1) * (x2 - x1)) + ((y2 - y1) * (y2 - y1));
+            Console.WriteLine(Math.Sqrt(LengthLine));
+
+        }
+
+    }
+}
---
+++ uc2.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_num1
+{
+    internal class UC2
+
+    {
+        int x1 = 2;
+        int x2 = 3;
+        int y1 = 4;
+        int y2 = 5;
+
+        double LengthLine1 = 0;
+
+        int a1 = 6;
+        int a2 = 7;
+        int b1 = 8;
+        int b2 = 9;
+
+        double LengthLine2 = 0;
+
+        public void CartesianSystem()
+        {
+            LengthLine1 = ((x2 - x1) * (x2 - x1)) + ((y2 - y1) * (y2 - y1));
+            Console.WriteLine(Math.Sqrt(LengthLine1));
+
+            LengthLine2 = ((a2 - a1) * (a2 - a1)) + ((b2 - b1) * (b2 - b1));
+            Console.WriteLine(Math.Sqrt(LengthLine2));
+
+            Console.WriteLine(LengthLine1.Equals(LengthLine2));
+
+           
+        }
+
+        }
+    }
